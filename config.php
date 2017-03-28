ce2afe0ed462037e51ac7850b6f3037b1fd38fc9
githubjeka <et.coder@gmail.com>
2017-03-28 13:51:15 +0300

    remote DashboardContent::EVENT_CREATE
    This event handle by StreamViewer::EVENT_CREATE already

--- config.php
+++ config.php
@@ -4,7 +4,6 @@
  * @author Evgeniy Tkachenko <et.coder@gmail.com>
  */
 
-use humhub\modules\dashboard\widgets\DashboardContent;
 use humhub\modules\space\widgets\Menu;
 use humhub\modules\stream\widgets\StreamViewer;
 
@@ -17,11 +16,6 @@ return [
             'event' => Menu::EVENT_INIT,
             'class' => Menu::className(),
             'callback' => [\tracker\Events::class, 'onSpaceMenuInit'],
-        ],
-        [
-            'event' => DashboardContent::EVENT_CREATE,
-            'class' => DashboardContent::className(),
-            'callback' => [\tracker\Events::class, 'onStreamViewerCreate'],
         ],
         [
             'event' => StreamViewer::EVENT_CREATE,
