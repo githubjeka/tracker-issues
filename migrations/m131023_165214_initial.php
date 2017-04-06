d38643b200d92484784f7ca165cea3545d207a91
Sarah Tsumayoi <Felli@users.noreply.github.com>
2017-04-06 00:27:07 -0400

    Update m131023_165214_initial.php

--- migrations/m131023_165214_initial.php
+++ migrations/m131023_165214_initial.php
@@ -12,15 +12,15 @@ class m131023_165214_initial extends \yii\db\Migration
             'title' => $this->string(255),
             'description' => $this->text(),
             'deadline' => $this->dateTime(),
-            'status' => $this->smallInteger()->notNull()->defaultValue(10),
-            'priority' => $this->smallInteger()->notNull()->defaultValue(100),
+            'status' => $this->smallInteger()-> not Null()->defaultValue(10),
+            'priority' => $this->smallInteger()-> not Null()->defaultValue(100),
         ], '');
 
         $this->createTable('tracker_assignee', [
             'id' => $this->primaryKey(),
-            'user_id' => $this->integer()->notNull(),
-            'issue_id' => $this->integer()->notNull(),
-            'view_mark' => $this->boolean()->notNull()->defaultValue(0),
+            'user_id' => $this->integer()-> not Null(),
+            'issue_id' => $this->integer()-> not Null(),
+            'view_mark' => $this->boolean()-> not Null()->defaultValue(0),
             'finish_mark' => $this->smallInteger()->defaultValue(0),
         ], '');
     }
