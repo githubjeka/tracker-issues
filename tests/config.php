d8ba9cb8ab977fdae969f531f9bd6d80700f9eb1
githubjeka <et.coder@gmail.com>
2017-04-28 15:13:29 +0300

    Fix config test

--- tests/config.php
+++ tests/config.php
@@ -4,12 +4,17 @@ return yii\helpers\ArrayHelper::merge(
     require(__DIR__ . '/../../../humhub/config/common.php'),
     [
         'id' => 'tracker-tests',
+        'controllerMap' => [
+            'installer' => 'humhub\modules\installer\commands\InstallController',
+        ],
         'components' => [
             'mailer' => [
                 'useFileTransport' => true,
             ],
             'urlManager' => [
                 'showScriptName' => true,
+                'baseUrl' => 'http://localhost:8080',
+                'hostInfo' => 'http://localhost:8080',
             ],
             'cache' => [
                 'class' => 'yii\caching\DummyCache',
