9f03ab7f5e1a1a96b0e709047b05236d1b5bfc1b
githubjeka <et.coder@gmail.com>
2017-05-02 09:05:50 +0300

    Add test to TagDesignator service

---
+++ tests/unit/TagServiceTest.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace tracker\tests\unit;
+
+use humhub\modules\space\models\Space;
+use humhub\modules\user\models\User;
+use tracker\controllers\IssueCreator;
+use tracker\controllers\IssueRequest;
+use tracker\controllers\TagDesignator;
+
+/**
+ * @author Evgeniy Tkachenko <et.coder@gmail.com>
+ * @property \tracker\UnitTester tester
+ */
+class TagServiceTest extends ServiceTest
+{
+    /**
+     * @Override
+     */
+    protected function _before()
+    {
+        $this->tester->haveFixtures([
+            'node' => \tracker\tests\fixtures\IssueFixture::class,
+            'space' => \humhub\modules\space\tests\codeception\fixtures\SpaceFixture::class,
+        ]);
+
+        $spaceContent = Space::findOne(['id' => 1]);
+        $creator = new IssueCreator();
+        $creator->createDraft($spaceContent);
+        $creator->load(['title' => 'My test issue'], '');
+        $issue = $creator->create();
+
+        $this->service = new TagDesignator($issue);
+        \Yii::$app->user->switchIdentity(User::findOne(['id' => 1]));
+    }
+
+    public function testInitService()
+    {
+        $requestForm = $this->service->getIssueForm();
+        $this->assertInstanceOf(IssueRequest::class, $requestForm);
+        $this->assertEquals(1, $requestForm->id);
+        $this->assertEquals([], $requestForm->tags);
+    }
+}
