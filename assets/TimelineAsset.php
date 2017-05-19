416d17a1caf9d324091e1f2d38ea94bf9e43503e
githubjeka <et.coder@gmail.com>
2017-05-19 16:03:22 +0300

    Added NUKnightLab/TimelineJS3 asset

---
+++ assets/TimelineAsset.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace tracker\assets;
+
+use yii\web\AssetBundle;
+
+/**
+ * @author Evgeniy Tkachenko <et.coder@gmail.com>
+ */
+class TimelineAsset extends AssetBundle
+{
+    public $sourcePath = __DIR__ . '/../resources/timeline/';
+
+    public $css = [
+        'timeline.css',
+    ];
+
+    public $js = [
+        'timeline.js',
+    ];
+
+    public $depends = [
+        IssueAsset::class,
+    ];
+}
