6f27e9f76110c021767a019058e9cde2a26f0a3f
Joe2824 <joel_klein@me.com>
2017-06-14 09:57:36 +0200

    Create base.php
    add translation

--- messages/de/base.php
+++ messages/de/base.php
@@ -17,6 +17,6 @@
  * NOTE: this file must be saved in UTF-8 encoding.
  */
 return [
-    'Issue' => '',
-    'Tracker issues' => '',
+    'Issue' => 'Problem',
+    'Tracker issues' => 'Bug Tracker',
 ];
