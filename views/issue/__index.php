fbd526a281ec707701284ff7f22b8da2e01cfe7c
githubjeka <et.coder@gmail.com>
2020-07-20 10:11:45 +0300

    Added sort issues by "deadline"

--- views/issue/__index.php
+++ views/issue/__index.php
@@ -4,25 +4,45 @@
  */
 
 /**
- * @var $this \humhub\components\View
- * @var \yii\data\ActiveDataProvider $dataProvider
- * @var \tracker\models\IssueSearch|null $searchModel
- * @var \humhub\modules\content\components\ContentContainerActiveRecord $contentContainer
+ * @var $this View
+ * @var ActiveDataProvider $dataProvider
+ * @var IssueSearch|null $searchModel
+ * @var ContentContainerActiveRecord $contentContainer
  */
 $formatter = Yii::$app->formatter;
+
+use humhub\components\View;
+use humhub\modules\content\components\ContentContainerActiveRecord;
+use tracker\models\IssueSearch;
+use yii\data\ActiveDataProvider;
+use yii\helpers\Html;
+use yii\helpers\Url;
+use yii\widgets\ListView;
+
 ?>
 <div class="row">
-    <div class="col-md-3">
-        <?php if ($searchModel !== null) : ?>
-            <?= $this->render('__search_index', ['searchModel' => $searchModel]) ?>
-        <?php endif; ?>
+  <div class="col-md-3">
+      <?php if ($searchModel !== null) : ?>
+          <?= $this->render('__search_index', ['searchModel' => $searchModel]) ?>
+      <?php endif; ?>
+  </div>
+  <div class="col-md-9">
+    <div class="text-center">
+        <?= Html::a(
+            Yii::t('TrackerIssuesModule.views', 'old first'),
+            Url::current(['sort' => 'deadline']),
+            ['class' => 'btn btn-link']
+        ) ?>
+        <?= Html::a(Yii::t('TrackerIssuesModule.views', 'new first'),
+            Url::current(['sort' => '-deadline']),
+            ['class' => 'btn btn-link']
+        ) ?>
     </div>
-    <div class="col-md-9">
-        <?= \yii\widgets\ListView::widget([
-            'dataProvider' => $dataProvider,
-            'itemView' => '__item_index',
-            'emptyText' => Yii::t('TrackerIssuesModule.views', 'No open issues...'),
-        ]) ?>
-    </div>
+      <?= ListView::widget([
+          'dataProvider' => $dataProvider,
+          'itemView' => '__item_index',
+          'emptyText' => Yii::t('TrackerIssuesModule.views', 'No open issues...'),
+      ]) ?>
+  </div>
 </div>
 
